aea9c4c3b7dec60163e7e12958f1e2e72d27bcb0
lukebarnard <luke.barnard99@gmail.com>
2015-02-08 12:55:30 +0100

    Architecture for server model created. All content is stored in one table. Perhaps it would be best to allow multiple types of content - not just text. This would require each content to have a file name ending in a useful file type such as '.txt' or '.png' so that the reader can display it appropriately. Each text post would just be a text file. But now the data model becomes a file system with people making links between files. Why not. Back to the drawing board...

--- server/o2m/basic_server/models.py
+++ server/o2m/basic_server/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+MODEL ARCHITECTURE
+
+Friend
+	- Name
+	- IP Address
+	- Password
+
+Content
+	- Content text
+	- Creation time
+	- Integer: Non-descript to allow variations
+
+Link
+	- Friend (Could be yourself)
+	- Content (Could be your own)
+	- Link: to the previous link in the tree
+
+'''
