a69aae0f9ecab99f3236fad3cd8a0cf9fd7a2fa6
lukebarnard <luke.barnard99@gmail.com>
2015-05-21 16:47:43 +0200

    Changes needed for setuptools

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 setup(
 	name = "O2M",
 	version = "0.2.0",
-	packages = find_packages(),
-	install_requires = ["django==1.8.1", "django-bootstrap3==5.4.0", "django-mptt==0.7.3", "django-mptt-admin==0.2.1"]
-
+	install_requires = ["django==1.8.1", "django-bootstrap3==5.4.0", "django-mptt==0.7.3", "django-mptt-admin==0.2.1"],
+	scripts = ['server/o2m/run_o2m.py'],
+	packages = ['o2m'],
 	package_data = {
-		'': ['*.db.sqlite3', '*.html'],
+		'': ['*.db.sqlite3', '*.html', '*.py'],
 	},
 
 	# metadata for upload to PyPI
