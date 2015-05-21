f1bd2f282ad91fa0874ed95c8b9b5a711e60c971
lukebarnard <luke.barnard99@gmail.com>
2015-05-21 16:35:22 +0200

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 setup(
-    name = "O2M",
-    version = "0.2.0",
-    packages = find_packages(),
+	name = "O2M",
+	version = "0.2.0",
+	packages = find_packages(),
+	install_requires = ["django==1.8.1", "django-bootstrap3==5.4.0", "django-mptt==0.7.3", "django-mptt-admin==0.2.1"]
 
-    package_data = {
-        '': ['*.db.sqlite3', '*.html'],
-    },
+	package_data = {
+		'': ['*.db.sqlite3', '*.html'],
+	},
 
-    # metadata for upload to PyPI
-    author = "Luke Barnard",
-    author_email = "luke.barnard99@gmail.com",
-    description = "One to Many - The Fully Distributed Secure Social Network",
-    license = "GNU GENERAL PUBLIC LICENSE v2",
-    keywords = "social network distributed",
-    url = "https://github.com/lukebarnard1/o2m",
+	# metadata for upload to PyPI
+	author = "Luke Barnard",
+	author_email = "luke.barnard99@gmail.com",
+	description = "One to Many - The Fully Distributed Secure Social Network",
+	license = "GNU GENERAL PUBLIC LICENSE v2",
+	keywords = "social network distributed",
+	url = "https://github.com/lukebarnard1/o2m",
 )
