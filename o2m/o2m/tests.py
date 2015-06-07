464864eaf56cceb80f8e03ef80ec193a830586f9
lukebarnard <luke.barnard99@gmail.com>
2015-06-07 14:47:01 +0200

    Added notification type test, using non-empty db

--- o2m/o2m/tests.py
+++ o2m/o2m/tests.py
@@ -2,13 +2,14 @@
 import unittest
 
 import o2m.settings
-o2m.settings.DATABASES['default']['NAME'] = 'db.sqlite3' # Use a local, accessible database
+o2m.settings.DATABASES['default']['NAME'] = 'o2m/db.sqlite3' # Use a local, accessible database
 
 import os, django
 os.environ["DJANGO_SETTINGS_MODULE"] = "o2m.settings"
 django.setup()
 
 from django.contrib.auth.models import User
+from o2m.basic_server.models import NotificationType
 
 from o2m import setup_initial_database
 setup_initial_database()
@@ -24,4 +25,9 @@ class TestInitialDatabase(unittest.TestCase):
 	def test_default_user(self):
 		'''Make sure there is a default User in the database'''
 		self.assertTrue(len(User.objects.filter(username='user', is_staff=True, is_superuser=True)) > 0)
+
+	def test_notification_types(self):
+		'''Make sure there are some notification types in the database'''
+		print len(NotificationType.objects.all())
+		self.assertTrue(len(NotificationType.objects.all()) == 2)
 
